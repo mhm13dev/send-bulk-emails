9e3893b939f966bb1f45ce43b065bb85ccbb519b
Mubashir Hassan <official.mhm13@gmail.com>
2022-01-19 18:18:52 +0500

    FIXED: filename for saving

--- index.js
+++ index.js
@@ -21,23 +21,23 @@ if (process.env.NODE_ENV === 'production') {
   client = nodemailer.createTransport(sgTransport(options));
   users = realUsers;
 } else {
-    options = {
-        host: process.env.MAILTRAP_HOST,
-        port: parseInt(process.env.MAILTRAP_PORT),
-        auth: {
-            user: process.env.MAILTRAP_USER,
-            pass: process.env.MAILTRAP_PASS,
-        },
-    };
-    client = nodemailer.createTransport(options);
-    users = testUsers;
+  options = {
+    host: process.env.MAILTRAP_HOST,
+    port: parseInt(process.env.MAILTRAP_PORT),
+    auth: {
+      user: process.env.MAILTRAP_USER,
+      pass: process.env.MAILTRAP_PASS,
+    },
+  };
+  client = nodemailer.createTransport(options);
+  users = testUsers;
 }
 
 const email = {
-    from: process.env.EMAIL_BY,
-    subject: 'Hello',
-    text: 'Hello world',
-    html: '<b>Hello world</b>',
+  from: process.env.EMAIL_BY,
+  subject: 'Hello',
+  text: 'Hello world',
+  html: '<b>Hello world</b>',
 };
 
 async function sendMail() {
@@ -51,7 +51,13 @@ async function sendMail() {
       users[i].mail_sent = true;
       users[i].messageId = res.messageId;
       fs.writeFileSync(
-        path.join(__dirname, 'data', 'users.json'),
+        path.join(
+          __dirname,
+          'data',
+          process.env.NODE_ENV !== 'production'
+            ? 'test_users.json'
+            : 'real_users.json'
+        ),
         JSON.stringify(users)
       );
       console.log('Mail sent');
